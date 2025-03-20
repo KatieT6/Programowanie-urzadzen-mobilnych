918adb3b3ee2c829fb1b77507b9978f949003fb4
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2025-03-20 16:55:58 +0100

    build succeeded + tests completed

--- Mobilki/Program.cs
+++ Mobilki/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Mobilki
+{
+    public class Kalkulator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HI!");
+        }
+
+        public static int Dodawanie(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Odejmowanie(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mnozenie(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Dzielenie(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("NIE DZIEL PRZEZ 0!");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Testy/Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mobilki;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testy
+{
+    [TestClass()]
+    public class KalkulatorTests
+    {
+        [TestMethod()]
+        public void DodawanieTest()
+        {
+            Assert.AreEqual(Kalkulator.Dodawanie(3, 2), 5);
+            Assert.AreNotEqual(Kalkulator.Dodawanie(3, 3), 5);
+        }
+
+        [TestMethod()]
+        public void OdejmowanieTest()
+        {
+            Assert.AreEqual(Kalkulator.Odejmowanie(3, 2), 1);
+            Assert.AreNotEqual(Kalkulator.Odejmowanie(3, 3), 5);
+        }
+
+        [TestMethod()]
+        public void MnozenieTest()
+        {
+            Assert.AreEqual(Kalkulator.Mnozenie(3, 2), 6);
+            Assert.AreNotEqual(Kalkulator.Mnozenie(3, 3), 4);
+        }
+
+        [TestMethod()]
+        public void DzielenieTest()
+        {
+            Assert.AreEqual(Kalkulator.Dzielenie(6, 2), 3);
+            Assert.AreNotEqual(Kalkulator.Dzielenie(3, 3), 2);
+        }
+    }
+}

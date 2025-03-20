0da9cc6c5f66bd7bd81846781f3686d50420d7a4
KatieT6 <91150647+KatieT6@users.noreply.github.com>
2025-03-20 17:29:00 +0100

    test project fix

--- Test/Test1.cs
+++ Test/Test1.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Mobilki;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Mobilki;
 
-namespace Testy
+namespace Test
 {
-    [TestClass()]
-    public class KalkulatorTests
+    [TestClass]
+    public sealed class Test1
     {
         [TestMethod()]
         public void DodawanieTest()

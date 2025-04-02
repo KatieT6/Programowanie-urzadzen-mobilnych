a0cf932b05e8c66f15bb518b2d5f69ccd46f2064
Paweł Stus <pawel.j.stus@gmail.com>
2025-04-02 19:07:50 +0200

    Library and DataLayer Tests

--- DataTest/BookTest.cs
+++ DataTest/BookTest.cs
@@ -1,48 +1,47 @@
 using Data;
 using System.Net.Mail;
 
-namespace Data
+namespace DataTest;
+
+[TestClass]
+public class BookTest
 {
-    [TestClass]
-    public class BookTest
+    [TestMethod]
+    public void ConstructorTest_Default()
     {
-        [TestMethod]
-        public void ConstructorTest_Default()
-        {
-            var book = IBook.CreateBook();
+        var book = IBook.CreateBook();
 
-            // Assert
-            Assert.IsNotNull(book);
-            Assert.AreEqual(string.Empty, book.Title);
-            Assert.AreEqual(string.Empty, book.Author);
-            Assert.AreEqual(0, book.Year);
-            Assert.AreEqual(BookType.NonFiction, book.Type);
-        }
+        // Assert
+        Assert.IsNotNull(book);
+        Assert.AreEqual(string.Empty, book.Title);
+        Assert.AreEqual(string.Empty, book.Author);
+        Assert.AreEqual(0, book.Year);
+        Assert.AreEqual(BookType.NonFiction, book.Type);
+    }
 
-        [TestMethod]
-        public void ConstructorTest_Arg()
-        {
-            var book = IBook.CreateBook("a", "b", 0, BookType.SciFi);
+    [TestMethod]
+    public void ConstructorTest_Arg()
+    {
+        var book = IBook.CreateBook("a", "b", 0, BookType.SciFi);
 
-            // Assert
-            Assert.IsNotNull(book);
-            Assert.AreEqual("a", book.Title);
-            Assert.AreEqual("b", book.Author);
-            Assert.AreEqual(0, book.Year);
-            Assert.AreEqual(BookType.SciFi, book.Type);
-        }
+        // Assert
+        Assert.IsNotNull(book);
+        Assert.AreEqual("a", book.Title);
+        Assert.AreEqual("b", book.Author);
+        Assert.AreEqual(0, book.Year);
+        Assert.AreEqual(BookType.SciFi, book.Type);
+    }
 
-        [TestMethod]
-        public void ConstructorTest_InitStruct()
-        {
-            BookInit bookInit = new BookInit("a", "b", 1, BookType.Romance);
-            var book = IBook.CreateBook(bookInit);
+    [TestMethod]
+    public void ConstructorTest_InitStruct()
+    {
+        BookInit bookInit = new BookInit("a", "b", 1, BookType.Romance);
+        var book = IBook.CreateBook(bookInit);
 
-            Assert.IsNotNull(book);
-            Assert.AreEqual("a", book.Title);
-            Assert.AreEqual("b", book.Author);
-            Assert.AreEqual(1, book.Year);
-            Assert.AreEqual(BookType.Romance, book.Type);
-        }
+        Assert.IsNotNull(book);
+        Assert.AreEqual("a", book.Title);
+        Assert.AreEqual("b", book.Author);
+        Assert.AreEqual(1, book.Year);
+        Assert.AreEqual(BookType.Romance, book.Type);
     }
 }
